7ce4e3221003ac4276dfed39bfdf0abb4209245a
Rumen Neshev <rstar2@abv.bg>
2023-10-27 10:44:26 +0300

    start PWA

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,7 +1,66 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    // plain React
+    react(),
+
+    // for PWA - https://vite-pwa-org.netlify.app/guide/
+    VitePWA({
+      //   registerType: "autoUpdate",
+      registerType: "prompt",
+      injectRegister: "auto",
+      includeAssets: ["favicon.ico", "apple-touc-icon.png", "masked-icon.svg"],
+
+      // dynamic manifest generation
+      manifest: {
+        name: "React-vite-app",
+        short_name: "react-vite-app",
+        description: "I am a simple vite app",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "favicon",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "favicon",
+          },
+          {
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "apple touch icon",
+          },
+          {
+            src: "/maskable_icon.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        theme_color: "#171717",
+        background_color: "#f0e7db",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait",
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
+});
